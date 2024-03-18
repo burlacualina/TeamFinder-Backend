c98253f5c079de91389367dca4f93c826fa54e30
andrei.doroftei <andrei.doroftei@assist.ro>
2024-03-18 11:14:18 +0200

    Added pipeline

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // (optional) Enable CORS if needed
-  await app.listen(5000);
+  await app.listen(8080);
 }
 bootstrap();
